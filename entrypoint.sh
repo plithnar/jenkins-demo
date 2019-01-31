90047ca3d2012f5f0bbc5fdba28f2430e75e34a2
Cloud User <centos@infra.s01.g-pnns2-gk02012019.stonedoorlabs.com>
2019-01-31 23:23:09 +0000

    updating entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-openssl s_client -connect ${DTR_IP}:443 -showcerts \
+openssl s_client -connect 209.97.150.102:443 -showcerts \
     </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee \
-    /usr/local/share/ca-certificates/${DTR_IP}.crt
+    /usr/local/share/ca-certificates/209.97.150.102.crt
 sudo update-ca-certificates
 
 /bin/tini -- /usr/local/bin/jenkins.sh
